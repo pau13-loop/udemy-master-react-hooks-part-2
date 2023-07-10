6ed848f5b2d178db23b995d458441991d56df736
Pau Llinas Amat <pllinas@intricom.es>
2023-07-10 14:30:58 +0200

    feat: :sparkles: Add MessageBoard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import reducer, { initialState } from "./state/reducer";
+import PublishMessage from "./components/PublishMessage";
+import MessageBoard from "./components/MessageBoard";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -9,6 +11,9 @@ function App() {
   return (
     <div>
       <h1>Reaction</h1>
+      <PublishMessage dispatch={dispatch} />
+      <hr />
+      <MessageBoard messages={state.messages} />
     </div>
   );
 }
---
+++ src/components/MessageBoard.js
@@ -0,0 +1,23 @@
+
+function MessageBoard({ messages }) {
+
+    return (
+        <div>
+            {
+                messages.map(messageItem => {
+                    const { id, text, timestamp } = messageItem;
+
+                    return (
+                        <div key={id}>
+                            <h4>{new Date(timestamp).toLocaleString()}</h4>
+                            <p>{text}</p>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
+export default MessageBoard;
